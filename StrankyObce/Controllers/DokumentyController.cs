06bfc6112e8c1f64a6b1ef5d92a4fe97227b27db
xkolm036 <martykolarik@gmail.com>
2018-03-14 11:13:47 +0100

    Dokumenty controller add

---
+++ StrankyObce/Controllers/DokumentyController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace StrankyObce.Controllers
+{
+    public class DokumentyController : Controller
+    {
+        // GET: Dokumenty
+   
+
+        public ActionResult UpFrom()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            try
+            {
+                var model = Server.MapPath(@"~/Dokumenty/") + file.FileName;
+                if (file.ContentLength > 0)
+                {
+                    file.SaveAs(model);
+                    TempData["msg-succes"] = file.FileName + " byl uspěšně nahrán";
+
+                }
+                else
+                {
+                    TempData["msg-error"] = "Prazdný soubor nelze nahrát";
+                }
+
+            }
+            catch (System.NullReferenceException)
+            {
+                TempData["msg-error"] = "Nebyl zvolen žádný soubor";
+                return RedirectToAction("UpForm");
+
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+    }
+}
