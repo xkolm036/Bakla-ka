6b53a454eeaba56e3477b756a67aa70cb95573a3
xkolm036 <martykolarik@gmail.com>
2018-03-17 01:35:04 +0100

    Vyhledavani dokumentu - ajax

--- StrankyObce/Controllers/DokumentyController.cs
+++ StrankyObce/Controllers/DokumentyController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAcces.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,10 +12,28 @@ namespace StrankyObce.Controllers
     public class DokumentyController : Controller
     {
         // GET: Dokumenty
-   
+
 
         public ActionResult UpFrom()
         {
+
+            List<Files> down = new List<Files>();
+            List<Files> date = new List<Files>();
+
+            using (hrebec_dataEntities context = new hrebec_dataEntities())
+            {
+                var stazene = context.Files.OrderByDescending(i => i.Pocet_Stazeni).Take(7);
+                var pridany = context.Files.OrderByDescending(i => i.Datum_Nahrani).Take(7);
+
+                foreach (Files f in stazene)
+                    down.Add(f);
+
+                foreach (Files f in pridany)
+                    date.Add(f);
+            }
+
+            ViewBag.down = down;
+            ViewBag.date = date;
 
 
             return View();
@@ -22,33 +41,87 @@ namespace StrankyObce.Controllers
 
         public ActionResult Upload(HttpPostedFileBase file)
         {
+
             try
             {
 
-                string path = Server.MapPath(@"~/App_Data/Dokumenty/");
+
+                string path = Path.Combine(Server.MapPath(@"~/App_Data/Dokumenty/"), file.FileName);
                 if (file.ContentLength > 0)
                 {
-                    file.SaveAs(Path.Combine(path, file.FileName));
+
+                    file.SaveAs(path);
                     TempData["msg-succes"] = file.FileName + " byl uspěšně nahrán";
+                    using (hrebec_dataEntities context = new hrebec_dataEntities())
+                    {
+
+                        Files f = new Files();
+                        f.Cesta = path;
+                        f.Název = file.FileName;
+                        DateTime thisDay = DateTime.Today;
+                        f.Datum_Nahrani = DateTime.Parse(thisDay.ToString("D")); // jenom datum
+                        int byteCount = file.ContentLength;
+                        f.Velikost = byteCount;
+                        f.Pocet_Stazeni = 0;
+
+                        context.Files.Add(f);
+                        context.SaveChanges();
+                    }
 
                 }
                 else
                 {
                     TempData["msg-error"] = "Prazdný soubor nelze nahrát";
                 }
-
             }
             catch (System.NullReferenceException)
             {
-                TempData["msg-error"] = "Nebyl zvolen žádný soubor";
-                return RedirectToAction("UpForm");
 
             }
+
+
+
 
             return RedirectToAction("Index", "Home");
         }
 
 
+        public FileResult Download(int id)
+        {
+            using (hrebec_dataEntities contex = new hrebec_dataEntities())
+            {
+                Files f =new Files();
+                f = contex.Files.FirstOrDefault(x => x.ID == id);
+                f.Pocet_Stazeni += 1;
+                contex.SaveChanges();
+
+                byte[] fileBytes = System.IO.File.ReadAllBytes(f.Cesta);
+                var response = new FileContentResult(fileBytes, "application/octet-stream");
+                response.FileDownloadName = f.Název;
+          
+                return response;
+
+            }
+               
+        }
+
+        public ActionResult AjaxRequest(string text)
+        {
+            List<Files> filesFromDB = new List<Files>();
+            using (hrebec_dataEntities contex= new hrebec_dataEntities())
+            {
+
+                var DBfiles = contex.Files.Where(f => f.Název.ToLower().Contains(text.ToLower()));
+
+                foreach (Files f in DBfiles)
+                {
+                    filesFromDB.Add(f);
+                }
+
+            }
+
+                return View(filesFromDB);
+        }
 
     }
 }
